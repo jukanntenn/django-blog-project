417705b5365a5deaf14f46d929bc068710931185
yangxg <zmrenwu@163.com>
2020-09-20 17:20:49 +0900

    speed up testing

--- config/settings/test.py
+++ config/settings/test.py
@@ -7,10 +7,35 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS += [  # noqa
     "core.tests",
 ]
-DDF_NUMBER_OF_LAPS = 1
 
-# Email configuration
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# CACHES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "",
+    }
+}
+
+# PASSWORDS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+# DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "ATOMIC_REQUESTS": True,
+    }
+}
 
 ADMINS = [("admin", "admin@example.com")]
 MANAGERS = ADMINS
