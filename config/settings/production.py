804ffd9614b04462524d9c6d6b4cd4c7148907d0
yangxg <zmrenwu@163.com>
2020-09-21 00:24:03 +0900

    Add redis cache

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,6 +11,21 @@ DATABASES = {"default": env.db("DATABASE_URL")}  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
+# CACHES
+# ------------------------------------------------------------------------------
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": env("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            # Mimicing memcache behavior.
+            # http://jazzband.github.io/django-redis/latest/#_memcached_exceptions_behavior
+            "IGNORE_EXCEPTIONS": True,
+        },
+    }
+}
+
 # django anymail
 ANYMAIL = {
     "SENDGRID_API_KEY": os.environ.get("DJANGO_SENDGRID_API_KEY"),
