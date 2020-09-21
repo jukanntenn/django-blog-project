cabce4ea6b946d7da2209d032b0c091fca8059d2
yangxg <zmrenwu@163.com>
2020-09-21 16:05:56 +0900

    show debug toolbar when use docker

--- config/settings/local.py
+++ config/settings/local.py
@@ -5,8 +5,13 @@ DEBUG = True
 SECRET_KEY = "fake-secret-key-for-development"
 ALLOWED_HOSTS = ["*"]
 
-# debug toolbar
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "localhost"]
+if env("USE_DOCKER") == "yes":
+    import socket
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 
 DEBUG_TOOLBAR_CONFIG = {
     "JQUERY_URL": "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js",
