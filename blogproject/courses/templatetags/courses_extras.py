3499da5faf080b2aca4179eeb7ce1e8f842a1e56
yangxg <zmrenwu@163.com>
2020-10-02 22:39:33 +0900

    cache course toc (#36)

--- blogproject/courses/templatetags/courses_extras.py
+++ blogproject/courses/templatetags/courses_extras.py
@@ -1,5 +1,7 @@
 from core.utils import generate_rich_content
 from django import template
+from django.core.cache import cache
+from django.core.cache.utils import make_template_fragment_key
 from django.urls import reverse
 
 register = template.Library()
@@ -9,15 +11,19 @@ register = template.Library()
 def show_course_toc(course, current=None):
     material_list = course.material_set.all().values("id", "title", "body")
 
-    # attach toc to material
-    for material in material_list:
-        toc_url = reverse(
-            "courses:material_detail",
-            kwargs={"slug": course.slug, "pk": material["id"]},
-        )
-        material["toc"] = generate_rich_content(material["body"], toc_url=toc_url)[
-            "toc"
-        ]
+    key = make_template_fragment_key("course_toc", [course.pk])
+    result = cache.get(key)
+    # if not cached, regenerate the course's toc
+    if result is None:
+        # attach toc to material
+        for material in material_list:
+            toc_url = reverse(
+                "courses:material_detail",
+                kwargs={"slug": course.slug, "pk": material["id"]},
+            )
+            material["toc"] = generate_rich_content(material["body"], toc_url=toc_url)[
+                "toc"
+            ]
 
     context = {
         "material_list": material_list,
