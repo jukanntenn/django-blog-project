6414f2b842a08e7c2fcf02c4cdc3e7aaf4f1d495
yangxg <zmrenwu@163.com>
2020-10-08 21:14:22 +0900

    modify test setting

--- config/settings/test.py
+++ config/settings/test.py
@@ -40,3 +40,7 @@ DATABASES = {
 ADMINS = [("admin", "admin@example.com")]
 MANAGERS = ADMINS
 LANGUAGE_CODE = "en-us"
+
+WEBPACK_LOADER["DEFAULT"]["STATS_FILE"] = os.path.join(
+    BASE_DIR, "frontend", "webpack-test-stats.json"
+)
