a31bd32c7613f351817c52b6d920809973af6af1
jukanntenn <75071550+jukanntenn@users.noreply.github.com>
2021-04-11 19:55:49 +0800

    backup db to qcloud (#172)

--- config/settings/production.py
+++ config/settings/production.py
@@ -54,10 +54,9 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # DBBACKUP_STORAGE_OPTIONS = {
 #     "oauth2_access_token": env.str("DJANGO_DBBACKUP_DROPBOX_OAUTH2_ACCESS_TOKEN"),
 # }
-DBBACKUP_STORAGE = "django.core.files.storage.FileSystemStorage"
-DBBACKUP_STORAGE_OPTIONS = {"location": "/backup/"}
-DBBACKUP_CLEANUP_KEEP = 1
-DBBACKUP_CLEANUP_KEEP_MEDIA = 1
+DBBACKUP_STORAGE = "django_storage_qcloud.storage.QcloudStorage"
+DBBACKUP_CLEANUP_KEEP = 2
+DBBACKUP_CLEANUP_KEEP_MEDIA = 2
 
 CELERY_BROKER_URL = env.str("CELERY_BROKER_URL")
 
@@ -120,3 +119,14 @@ if SENTRY_DSN != "":
     )
 
 WATCHMAN_TOKENS = env.str("DJANGO_WATCHMAN_TOKENS")
+
+
+# django-storage-qcloud
+# ------------------------------------------------------------------------------
+# https://github.com/fordguo/django-storage-qcloud
+QCLOUD_STORAGE_OPTION = {
+    "SecretId": env.str("QCLOUD_STORAGE_OPTION_SECRET_ID"),
+    "SecretKey": env.str("QCLOUD_STORAGE_OPTION_SECRET_KEY"),
+    "Region": env.str("QCLOUD_STORAGE_OPTION_REGION"),
+    "Bucket": env.str("QCLOUD_STORAGE_OPTION_BUCKET"),
+}
