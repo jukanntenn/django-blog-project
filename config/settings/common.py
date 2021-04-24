a08be3937462911928aed2634b088089a3a0a9d3
thomas <>
2021-04-24 15:48:27 +0800

    change CELERY_TASK_SOFT_TIME_LIMIT from 60s to 5m

--- config/settings/common.py
+++ config/settings/common.py
@@ -341,7 +341,7 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_TIME_LIMIT = 5 * 60
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-soft-time-limit
 # todo: set to whatever value is adequate in your circumstances
-CELERY_TASK_SOFT_TIME_LIMIT = 60
+CELERY_TASK_SOFT_TIME_LIMIT = 5 * 60
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#beat-scheduler
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_BROKER_TRANSPORT_OPTIONS = {"max_retries": 3}
